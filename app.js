210e4f08d58a6bf26919c3e8f0f77097272b085c
87stal <87stal@gmail.com>
2021-08-19 16:48:35 +0300

    splitting code

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const folderBoys = path.join(__dirname, "boys");
 const folderGirls = path.join(__dirname, "girls");
 
 const sortUser = (pathToFolder) => {
+
   fs.readdir(pathToFolder)
     .then((files) =>
       files.forEach((file) => {
+
         const currentFilePath = path.join(pathToFolder, file);
         const girlsFilePath = path.join(folderGirls, file);
         const boysFilePath = path.join(folderBoys, file);
+
         fs.readFile(currentFilePath).then((userInfo) => {
+
           const user = JSON.parse(userInfo.toString());
+
           switch (user.gender) {
             case "female":
               fs.rename(currentFilePath, girlsFilePath)
