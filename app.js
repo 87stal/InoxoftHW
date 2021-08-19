73b0b82e32368b4297607f57fb3d36d47a8b3b09
87stal <87stal@gmail.com>
2021-08-19 14:28:22 +0300

    Added function changeHierarchy

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
-const fs = require("fs").promises;
+const fs = require('fs').promises
 const path = require("path");
 
-const folderBoys = path.join(__dirname, "boys");
-const folderGirls = path.join(__dirname, "girls");
+const mainFolder = path.join(__dirname, "folder");
 
-const sortUser = (pathToFolder) => {
+const changeHierarchy = (pathToFolder) => { 
   fs.readdir(pathToFolder)
-    .then((files) =>
-      files.forEach((file) => {
-        const currentFilePath = path.join(pathToFolder, file);
-        const girlsFilePath = path.join(folderGirls, file);
-        const boysFilePath = path.join(folderBoys, file);
-        fs.readFile(currentFilePath).then((userInfo) => {
-          const user = JSON.parse(userInfo.toString());
-          switch (user.gender) {
-            case "female":
-              fs.rename(currentFilePath, girlsFilePath)
-                .catch((err) =>
-                  console.log(err)
-                );
-              break;
-
-            case "male":
-              fs.rename(currentFilePath, boysFilePath)
-                .catch((err) =>
-                  console.log(err)
-                );
-              break;
-          }
-        });
-      })
-    )
-    .catch((err) => console.log(err));
-};
-
-sortUser(folderBoys);
-sortUser(folderGirls);
+  .then((files) => 
+  files.forEach( async (file)=> {   
+    const currentPath = path.join(pathToFolder, file); 
+    const stats = await fs.stat(currentPath);
+    const newPath = path.join(mainFolder, file);
+    if(stats.isDirectory){
+      changeHierarchy(currentPath)
+    }  
+    fs.rename(currentPath, newPath)
+    .catch((err) =>
+    console.log(err)
+  );
+  }))
+}
+changeHierarchy(mainFolder);
