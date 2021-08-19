50a08e26b53b4cae64c4154b9f6a971a15934840
87stal <87stal@gmail.com>
2021-08-19 16:46:04 +0300

    splitting code

--- app.js
+++ app.js
@@ -4,21 +4,28 @@ const path = require("path");
 const mainFolder = path.join(__dirname, "folder");
 
 const changeHierarchy = (pathToFolder) => { 
+ 
   fs.readdir(pathToFolder)
   .then((files) => 
   files.forEach( async (file)=> {   
+
     const currentPath = path.join(pathToFolder, file); 
     const stats = await fs.stat(currentPath);
     const newPath = path.join(mainFolder, file);
+
     if(stats.isDirectory()){
       changeHierarchy(currentPath);
     }  
+
     fs.rename(currentPath, newPath)
     .catch((err) =>
     console.log(err)
   );
+
   }))
   .catch((err) =>
   console.log(err))
 }
+
+
 changeHierarchy(mainFolder);
